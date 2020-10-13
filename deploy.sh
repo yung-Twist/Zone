f2e94bbd4ec782890e4d618721a218039cc99116
yanghh <402745326@qq.com>
2020-10-13 10:24:12 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ cd docs/.vuepress/dist
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init
 git add -A
 git commit -m 'deploy'
 
@@ -20,6 +19,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yung-Twist/Zone.git master:gh-pages
-
+git push -f git@github.com:yung-Twist/Zone.git main:gh-pages
 cd -
